354257a06501235a7617b415526317169c0a6c47
Daniel Röhrs <roehrsdaniel@gmail.com>
2021-02-20 21:22:38 +0100

    implemented deletion of draft embed via emoji

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from src import dicts
+
 
 class Events(commands.Cog):
     def __init__(self, bot):
@@ -9,6 +11,12 @@ class Events(commands.Cog):
     async def on_ready(self):
         print('Logged in as {0.user}'.format(self.bot))
         print('------')
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if (reaction.emoji == "❌") and (user.id != self.bot.user.id):
+            await reaction.message.delete()
+            await reaction.message.channel.send(dicts.DRAFT_RESET, delete_after=5)
 
 
 def setup(bot):
