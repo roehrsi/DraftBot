63affe9841ea3e2235501527a5d806a479a6ceac
Daniel Röhrs <roehrsdaniel@gmail.com>
2021-03-10 16:54:41 +0100

    changed map and hero pool to alias based system. altered util accordingly

--- src/util.py
+++ src/util.py
@@ -2,31 +2,41 @@ import json
 import regex
 
 
-def isin(kind: str, arg1, arg2=None) -> list:
+def isin(kind: str, arg1: str, arg2: str = None) -> list:
+    """Searches for the given arguments in either hero or map pool (depends on 'kind') and returns one unique name for
+    each argument.
+    Utilizes fuzzy regex for arguments of sufficient length, so typos are sometimes overlooked.
+    Don't expect AI miracles here...
+    The pool of maps and heroes works with aliases, so some common alternative spellings or shorthands are accepted.
+    If the Number of confirmed entries does not equal the arguments given, returns empty.
+    Otherwise return a list of confirmed entries."""
+
     if kind == "map" or "hero":
         with open("draft.json", "r") as res:
             jfile = json.load(res)
-            entries = jfile[kind]
-
+            pool = jfile[kind]
         if arg2:
             args = "".join(arg1, arg2) if kind == "map" else (arg1, arg2)
         else:
             args = [arg1]
 
         print(f"args: {args}")
-        match=[]
+        match = []
         result = []
+        # iterate over aliases for pool elements and try to regex match to arguments
         for arg in args:
+            # pattern fuzz depends on argument length. floordiv 4 seems like a good baseline for this...
             fuzz = len(arg) // 4
             pattern = r"({0}){{e<={1}}}".format(arg, fuzz)
-            # pattern = r"{0}".format(arg)
-            match.extend([entry for entry in entries if regex.search(pattern, entry, regex.BESTMATCH | regex.IGNORECASE)])
-            print(f"match: {match}")
-            print(len(match))
-        if len(match) > len(args):
-            for arg in args:
-                result.extend([m for m in match if regex.search(arg, m, regex.BESTMATCH | regex.IGNORECASE)])
-        else:
+            for entry in pool:
+                for alias in entry["alias"]:
+                    # add entry, if any aliases match the argument
+                    if regex.search(pattern, alias, regex.IGNORECASE):
+                        match.append(entry["name"])
+                        break
+        # assert one result for each argument
+        if len(match) == len(args):
             result = match
+
         print(f"final match: {result}")
         return result
