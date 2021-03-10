6c2930aab50faadd612873d9e5b44964ebe353e8
Daniel Röhrs <roehrsdaniel@gmail.com>
2021-03-10 20:47:15 +0100

    restructured data class to allow for custom draft structure. to be enhanced

--- src/data.py
+++ src/data.py
@@ -1,12 +1,10 @@
-SECOND_MAP_BANS = [0, 2]
-FIRST_MAP_BANS = [1, 3]
-SECOND_MAP_PICK = [4]
-FIRST_HERO_BANS = [5, 7, 15]
-SECOND_HERO_BANS = [6, 8, 14]
-FIRST_HERO_PICKS = [9, 12, 13, 18, 19]
-SECOND_HERO_PICKS = [10, 11, 16, 17, 20]
-FIRST_PICK = [9]
-LAST_PICK = [20]
+SECOND_MAP_BANS = 0
+FIRST_MAP_BANS = 1
+MAP_PICK = 2
+FIRST_HERO_BANS = 3
+SECOND_HERO_BANS = 4
+FIRST_HERO_PICKS = 5
+SECOND_HERO_PICKS = 6
 
 
 class Team:
@@ -36,60 +34,107 @@ class Team:
 
 
 class Draft:
-    def __init__(self, team_first=None, team_second=None, stage_num=0):
-        self.stage_num = stage_num
+    """The Draft instance that will be used to keep track of the active draft"""
+
+    def __init__(self, team_first=None, team_second=None, stage_num=0, mb_num: int = 2, tm_num: int = 5):
+        self.stage_num = 0
         self.team_first = team_first
         self.team_second = team_second
+        self.num_map_bans = mb_num if mb_num in range(0, 8) else 2
+        self.num_team_members = tm_num if tm_num in range(1, 6) else 5
+        # init stages
+        self.SECOND_MAP_BANS = [*range(0, mb_num * 2, 2)]
+        self.FIRST_MAP_BANS = [*range(1, mb_num * 2, 2)]
+        self.MAP_PICK = self.num_map_bans * 2
+        self.FIRST_HERO_BANS = [*range(self.MAP_PICK, self.MAP_PICK + 4, 2)]
+        self.SECOND_HERO_BANS = [*range(self.MAP_PICK + 1, self.MAP_PICK + 5, 2)]
+        self.FIRST_PICK = self.SECOND_HERO_BANS + 1
+        if self.num_team_members == 5:
+            self.FIRST_HERO_PICKS = [self.FIRST_PICK, self.FIRST_PICK + 3, self.FIRST_PICK + 4, self.FIRST_PICK + 9,
+                                     self.FIRST_PICK + 10]
+            self.SECOND_HERO_PICKS = [self.FIRST_PICK + 1, self.FIRST_PICK + 2, self.FIRST_PICK + 7,
+                                      self.FIRST_PICK + 8, self.FIRST_PICK + 11]
+            self.SECOND_HERO_BANS = self.SECOND_HERO_BANS + [self.FIRST_PICK + 5]
+            self.FIRST_HERO_BANS = self.FIRST_HERO_BANS + [self.FIRST_PICK + 6]
+        elif self.num_team_members == 4:
+            pass
+        elif self.num_team_members == 3:
+            self.FIRST_HERO_PICKS = [self.FIRST_PICK, self.FIRST_PICK + 5, self.FIRST_PICK + 6]
+            self.SECOND_HERO_PICKS = [self.FIRST_PICK + 1, self.FIRST_PICK + 2, self.FIRST_PICK + 7]
+            self.SECOND_HERO_BANS = self.SECOND_HERO_BANS + [self.FIRST_PICK + 3]
+            self.FIRST_HERO_BANS = self.FIRST_HERO_BANS + [self.FIRST_PICK + 4]
+        elif self.num_team_members == 2:
+            pass
+        elif self.num_team_members == 1:
+            self.FIRST_HERO_PICKS = self.FIRST_PICK
+            self.SECOND_HERO_PICKS = self.FIRST_PICK + 1
+
+        # init teams
         if team_first is None:
             self.team_first = Team("", [], "", [], [])
         if team_second is None:
             self.team_second = Team("", [], "", [], [])
 
+    def set_num_map_bans(self, num: int):
+        self.num_map_bans = num
+
+    def set_num_team_members(self, num: int):
+        self.num_team_members = num
+
     def turn(self) -> int:
-        """This will return the Team that is currently expected to ban or pick.
-        Returns 1 for the first pick team and 0 for the map pick team"""
-        if self.stage_num in (FIRST_HERO_PICKS + FIRST_HERO_BANS + FIRST_MAP_BANS):
-            return 1
-        else:
-            return 0
+        """This will return the stage that is currently happening."""
+        if self.stage_num in self.FIRST_HERO_PICKS:
+            return FIRST_HERO_PICKS
+        elif self.stage_num in self.SECOND_HERO_PICKS:
+            return SECOND_HERO_PICKS
+        elif self.stage_num in self.FIRST_HERO_BANS:
+            return FIRST_HERO_BANS
+        elif self.stage_num in self.SECOND_HERO_BANS:
+            return SECOND_HERO_BANS
+        elif self.stage_num in self.MAP_PICK:
+            return MAP_PICK
+        elif self.stage_num in self.FIRST_MAP_BANS:
+            return FIRST_MAP_BANS
+        elif self.stage_num in self.SECOND_MAP_BANS:
+            return SECOND_MAP_BANS
 
     def lock(self, picks):
         """lock in the pick or ban into the current draft"""
         print(f"stage: {self.stage_num}")
         # ban maps
-        if self.stage_num in SECOND_MAP_BANS:
+        if self.turn() == SECOND_MAP_BANS:
             self.team_second.map_bans.append(picks[0])
             self.stage_num += 1
             return
-        elif self.stage_num in FIRST_MAP_BANS:
+        elif self.turn() == FIRST_MAP_BANS:
             self.team_first.map_bans.append((picks[0]))
             self.stage_num += 1
             return
         # pick map
-        elif self.stage_num in SECOND_MAP_PICK:
+        elif self.turn() == MAP_PICK:
             self.team_second.map_pick = picks[0]
             self.stage_num += 1
             return
         # ban heroes
-        elif self.stage_num in FIRST_HERO_BANS:
+        elif self.turn() == FIRST_HERO_BANS:
             self.team_first.hero_bans.append(picks[0])
             self.stage_num += 1
             return
-        elif self.stage_num in SECOND_HERO_BANS:
+        elif self.turn() == SECOND_HERO_BANS:
             self.team_second.hero_bans.append(picks[0])
             self.stage_num += 1
             return
         # pick heroes
-        elif self.stage_num in FIRST_HERO_PICKS:
-            if self.stage_num in FIRST_PICK:
+        elif self.turn() == FIRST_HERO_PICKS:
+            if self.stage_num in self.FIRST_PICK:
                 self.team_first.hero_picks.append(picks[0])
                 self.stage_num += 1
             else:
                 self.team_first.hero_picks.extend(picks)
                 self.stage_num += len(picks)
             return
-        elif self.stage_num in SECOND_HERO_PICKS:
-            if self.stage_num in LAST_PICK:
+        elif self.turn() == SECOND_HERO_PICKS:
+            if self.stage_num in self.SECOND_HERO_PICKS[-1]:
                 self.team_second.hero_picks.append(picks[0])
                 self.stage_num += 1
             else:
