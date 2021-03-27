382454eb2888cce83b82dc74db901c7087b57117
Daniel Röhrs <roehrsdaniel@gmail.com>
2021-03-27 18:36:30 +0100

    fixed arg concat

--- src/util.py
+++ src/util.py
@@ -16,7 +16,7 @@ def isin(kind: str, arg1: str, arg2: str = None) -> list:
             jfile = json.load(res)
             pool = jfile[kind]
         if arg2:
-            args = str.lower("".join(arg1, arg2)) if kind == "map" else (str.lower(arg1), str.lower(arg2))
+            args = (" ".join((arg1.lower(), arg2.lower())) if kind == "map" else (arg1.lower(), arg2.lower()))
         else:
             args = [str.lower(arg1)]
 
@@ -25,7 +25,7 @@ def isin(kind: str, arg1: str, arg2: str = None) -> list:
         # iterate over aliases for pool elements and try to regex match to arguments
         for arg in args:
             # pattern fuzz depends on argument length. floordiv 4 seems like a good baseline for this...
-            fuzz = len(arg) // 4
+            fuzz = len(arg) // 5
             pattern = r"({0}){{e<={1}}}".format(arg, fuzz)
             for entry in pool:
                 for alias in entry["alias"]:
@@ -33,12 +33,14 @@ def isin(kind: str, arg1: str, arg2: str = None) -> list:
                     if regex.search(pattern, alias, regex.IGNORECASE):
                         match.append(entry["name"]) if isinstance(entry["name"], str) else match.extend(entry["name"])
                         break
-        # assert at most 2 results.
         print(f"final match: {match}")
+        # assert at most 2 results.
         if len(match) > len(args):
+            # chogall is special case
             if match[:2] == ["Cho", "Gall"]:
                 return match[:2]
             else:
+                # else match to args exactly, if possible. returns none otherwise
                 result = [m for m in match if str.lower(m) in args]
                 print(f"result: {result}")
                 return result
